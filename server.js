c4013192398ce6e20fea2b3db0cd1ada73278943
unknown <dmitrijs.pavlovs123@gmail.com>
2018-07-10 15:17:55 +0300

    API Fix

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+var request = require('request');
 const app = express();
 
-// Serve only the static files form the dist directory
+var options = {
+  url: 'https://erply-challenge.herokuapp.com/list',
+  headers: {
+    AUTH: 'fae7b9f6-6363-45a1-a9c9-3def2dae206d'
+  }
+};
+
+app.route('/api/').get((req, res) => {
+  request(options, function(error, response, body) {
+    res.send(body);
+  });
+});
 app.use(express.static(__dirname + '/dist/erply-webshop'));
 
 app.get('/*', function(req, res) {
